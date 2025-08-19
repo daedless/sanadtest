052f7fef944e8c5361f4f3e78652bbd4b06ba0d5
daedless <143393110+daedless@users.noreply.github.com>
2025-08-19 18:59:14 +0300

    Delete js directory

--- js/scrollup.js
+++
@@ -1,6 +0,0 @@
-// scrioll up /////
-
-  document.querySelector('.back-to-top').addEventListener('click', function(e) {
-    e.preventDefault();
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  });
